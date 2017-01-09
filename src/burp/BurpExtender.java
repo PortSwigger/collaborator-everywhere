4ea904e09cc7261e2224edf8b345db6235a2f2f9
James Kettle <albinowax@gmail.com>
2017-01-09 11:12:01 +0000

    Avoid duplicate u=collaboratordomain parameters

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -224,7 +224,8 @@ class Injector implements IProxyListener {
         byte[] fixed;
 
         IParameter param = Utilities.helpers.buildParameter("u", "http://"+collab.generateCollabId(requestCode, "u param")+"/u", IParameter.PARAM_URL);
-        fixed = Utilities.helpers.addParameter(request, param);
+        fixed = Utilities.helpers.removeParameter(request, param);
+        fixed = Utilities.helpers.addParameter(fixed, param);
 
         fixed = Utilities.addOrReplaceHeader(fixed, "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 http://"+collab.generateCollabId(requestCode, "User-Agent")+"/ua");
 
